e1e71c69b76a8512602e6c4d2b78a347e322d7b8
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2025-12-02 10:20:16 +0800

    added-docker-compose

---
+++ docker-compose/app/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js behind Nginx!");
+});
+
+app.listen(3000, () => console.log("Node app running on port 3000"));
